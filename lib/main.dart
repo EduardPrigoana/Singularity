96451c5c334c1420ce3bdc2e1686c6d7b88dca01
Atin Bainada <atin4@proton.me>
2025-08-26 21:20:26 +0530

    rm receive_sharing_intent

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,9 @@ import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-// import 'package:just_audio_media_kit/just_audio_media_kit.dart';
-// import 'package:home_widget/home_widget.dart';
 import 'package:logging/logging.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 import 'package:singularity/Helpers/config.dart';
-import 'package:singularity/Helpers/handle_native.dart';
-import 'package:singularity/Helpers/import_export_playlist.dart';
 import 'package:singularity/Helpers/logging.dart';
 import 'package:singularity/Helpers/route_handler.dart';
 import 'package:singularity/Screens/Common/routes.dart';
@@ -22,16 +17,24 @@ import 'package:singularity/constants/constants.dart';
 import 'package:singularity/constants/languagecodes.dart';
 import 'package:singularity/localization/app_localizations.dart';
 import 'package:singularity/providers/audio_service_provider.dart';
+import 'package:singularity/src/rust/frb_generated.dart';
 import 'package:singularity/theme/app_theme.dart';
 import 'package:sizer/sizer.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // Paint.enableDithering = true; No longer needed
+
+  await RustLib.init();
+  await hiveInit();
+  await startService();
+
+  runApp(MyApp());
+}
+
+Future<void> hiveInit() async {
   if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
     final home = Platform.environment['HOME']!;
     Hive.init('$home/.local/share/Singularity/Database');
-    // JustAudioMediaKit.ensureInitialized();
   } else if (Platform.isIOS) {
     await Hive.initFlutter('Database');
   } else {
@@ -43,9 +46,6 @@ Future<void> main() async {
       limit: box['limit'] as bool? ?? false,
     );
   }
-
-  await startService();
-  runApp(MyApp());
 }
 
 Future<void> startService() async {
@@ -79,33 +79,6 @@ Future<void> openHiveBox(String boxName, {bool limit = false}) async {
   }
 }
 
-/// Called when Doing Background Work initiated from Widget
-// @pragma('vm:entry-point')
-// Future<void> backgroundCallback(Uri? data) async {
-//   if (data?.host == 'controls') {
-//     final audioHandler = await AudioHandlerHelper().getAudioHandler();
-//     if (data?.path == '/play') {
-//       audioHandler.play();
-//     } else if (data?.path == '/pause') {
-//       audioHandler.pause();
-//     } else if (data?.path == '/skipNext') {
-//       audioHandler.skipToNext();
-//     } else if (data?.path == '/skipPrevious') {
-//       audioHandler.skipToPrevious();
-//     }
-
-//     // await HomeWidget.saveWidgetData<String>(
-//     //   'title',
-//     //   audioHandler?.mediaItem.value?.title,
-//     // );
-//     // await HomeWidget.saveWidgetData<String>(
-//     //   'subtitle',
-//     //   audioHandler?.mediaItem.value?.displaySubtitle,
-//     // );
-//     // await HomeWidget.updateWidget(name: 'SingularityMusicWidget');
-//   }
-// }
-
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
@@ -129,8 +102,7 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    // HomeWidget.setAppGroupId('com.shadow.singularity');
-    // HomeWidget.registerBackgroundCallback(backgroundCallback);
+
     final String systemLangCode = Platform.localeName.substring(0, 2);
     final String? lang = Hive.box('settings').get('lang') as String?;
     if (lang == null &&
@@ -143,77 +115,6 @@ class _MyAppState extends State<MyApp> {
     AppTheme.currentTheme.addListener(() {
       setState(() {});
     });
-
-    if (Platform.isAndroid || Platform.isIOS) {
-      // For sharing or opening urls/text/files coming from outside the app while the app is in the memory
-      _intentDataStreamSubscription =
-          ReceiveSharingIntent.getMediaStream().listen(
-        (List<SharedMediaFile> value) {
-          if (value.isNotEmpty) {
-            Logger.root.info('Received intent on stream: $value');
-            for (final file in value) {
-              if (file.type == SharedMediaType.text ||
-                  file.type == SharedMediaType.url) {
-                handleSharedText(file.path, navigatorKey);
-              }
-              if (file.type == SharedMediaType.file) {
-                if (file.path.endsWith('.json')) {
-                  final List playlistNames = Hive.box('settings')
-                          .get('playlistNames')
-                          ?.toList() as List? ??
-                      ['Favorite Songs'];
-                  importFilePlaylist(
-                    null,
-                    playlistNames,
-                    path: file.path,
-                    pickFile: false,
-                  ).then(
-                    (value) =>
-                        navigatorKey.currentState?.pushNamed('/playlists'),
-                  );
-                }
-              }
-            }
-          }
-        },
-        onError: (err) {
-          Logger.root.severe('ERROR in getMediaStream', err);
-        },
-      );
-
-      // For sharing files coming from outside the app while the app is closed
-      ReceiveSharingIntent.getInitialMedia()
-          .then((List<SharedMediaFile> value) {
-        if (value.isNotEmpty) {
-          Logger.root.info('Received Intent initially: $value');
-          for (final file in value) {
-            if (file.type == SharedMediaType.text ||
-                file.type == SharedMediaType.url) {
-              handleSharedText(file.path, navigatorKey);
-            }
-            if (file.type == SharedMediaType.file) {
-              if (file.path.endsWith('.json')) {
-                final List playlistNames = Hive.box('settings')
-                        .get('playlistNames')
-                        ?.toList() as List? ??
-                    ['Favorite Songs'];
-                importFilePlaylist(
-                  null,
-                  playlistNames,
-                  path: file.path,
-                  pickFile: false,
-                ).then(
-                  (value) => navigatorKey.currentState?.pushNamed('/playlists'),
-                );
-              }
-            }
-          }
-          ReceiveSharingIntent.reset();
-        }
-      }).onError((error, stackTrace) {
-        Logger.root.severe('ERROR in getInitialMedia', error);
-      });
-    }
   }
 
   void setLocale(Locale value) {
